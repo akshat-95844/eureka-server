cf5d9a118d70ad3a1cb5d3c840a864e7218c28aa
Akshat Gangrade <akshatgangrade@Akshats-MacBook-Air.local>
2025-02-16 19:54:37 +0530

    added logs in the application

--- src/main/java/com/example/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/example/eureka_server/EurekaServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+	Logger.info("Added logger on the main application");
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
